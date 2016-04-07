cf253f746034345be8b3c5fd6cd1da8d739a33c2
Sergio Contreras Sustaita <sergio.contreras@triolabs.mx>
2016-04-07 18:45:45 -0500

    Add sessions table and completed the migrations faces

---
+++ database/migrations/2016_04_07_234041_create_sessions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function(Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sessions');
+    }
+}
